6a0d8302f1c5f713c77fde80bbbb879cbddb2997
= <najot.talim.foundation001@gamil.com>
2026-06-29 14:41:23 +0500

    tugadi

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import json
 with open("students.json", encoding="utf-8") as file:
     data = json.load(file)
 
-for student in data:
-    print(student.get("name"))
+# for student in data:
+#     print(student.get("name"))
 
-print("\n\nPythonda o'qiyotganlar:")
+# print("\n\nPythonda o'qiyotganlar:")
+# for student in data:
+#     courses = student.get("courses")
+#     if "Python" in courses:
+        # print(student.get("name"))
+        
+course_names = set()
 for student in data:
     courses = student.get("courses")
-    if "Python" in courses:
-        print(student.get("name"))
+    course_names.update(courses)
+
+counter = {}
+for c in course_names:
+    for student in data:
+        courses = student.get("courses")
+        if c in courses:
+            counter.setdefault(c, 0)
+            counter[c] += 1
+
+for name, count in sorted(counter.items()):
+    print(f"{name} - {count}")
