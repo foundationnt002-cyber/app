66686553d3324b10969696c0ea87472228038154
= <najot.talim.foundation001@gamil.com>
2026-06-29 14:23:24 +0500

    2-shart bajarildi

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ with open("students.json", encoding="utf-8") as file:
 
 for student in data:
     print(student.get("name"))
+
+print("\n\nPythonda o'qiyotganlar:")
+for student in data:
+    courses = student.get("courses")
+    if "Python" in courses:
+        print(student.get("name"))
